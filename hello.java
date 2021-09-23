eea99d5c6a600de6a67d5f4458e2ef83d21a5fa0
haomu-code <75708075+haomu-code@users.noreply.github.com>
2021-09-23 22:08:11 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String args[]){
 		System.out.println("hello world");
-		System.out.println("栋泽sb");
+		System.out.println("2xx");
 		System.out.println("first update222222");
 	}
 }
