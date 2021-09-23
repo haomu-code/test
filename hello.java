09885fa2a3af21eb52c857788068d5be836459b7
Hellotzt <76930592+Hellotzt@users.noreply.github.com>
2021-09-23 22:03:25 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String args[]){
 		System.out.println("hello world");
-		System.out.println("first update111111");
+		System.out.println("栋泽sb");
 		System.out.println("first update222222");
 	}
 }
