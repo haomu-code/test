dadef9c220936a5523bce024f664d605f068d7b9
haomu-code <fdz15959626529@163.com>
2021-09-24 14:28:04 +0800

    test hello insert Date

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
+import java.util.Date;
+
 public class hello{
-	public static void main(String args[]){
+	public static void main(String[] args){
 		System.out.println("hello world");
+		System.out.println("2xx");
 		System.out.println("first update222222");
+		System.out.println(new Date());
 	}
 }
