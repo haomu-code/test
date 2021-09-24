d8d0db9505eca9103972f873f9f25d6d1d42f102
haomu-code <fdz15959626529@163.com>
2021-09-24 14:23:51 +0800

    master hello insert Date

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
+import java.util.Date;
+
 public class hello{
 	public static void main(String args[]){
 		System.out.println("hello world");
 		System.out.println("2xx");
 		System.out.println("first update222222");
-		//git@github.com:haomu-code/test.git++++++333333333
+		System.out.println(new Date());
 	}
 }
