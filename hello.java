7edaa34da286769cc43322287ff289cb4fb395c8
haomu-code <fdz15959626529@163.com>
2021-09-24 14:24:37 +0800

    master hello insert Date

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 
 public class hello{
-	public static void main(String args[]){
+	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("2xx");
 		System.out.println("first update222222");
